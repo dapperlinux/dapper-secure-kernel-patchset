f775713ad4c2525ed33433f7911cbb8f990033ed
Matthew Ruffell <msr50@uclive.ac.nz>
2017-10-28 19:27:18 +1300

    Dapper Secure Kernel Patchset 4.13.10

--- init_rebase.sh
+++ init_rebase.sh
@@ -1,5 +1,5 @@
 KERNEL_MAJOR_VERSION=4.13
-KERNEL_VERSION=4.13.9
+KERNEL_VERSION=4.13.10
 
 echo "Setting up rebase directory..."
 mkdir rebase
--- test_patch.sh
+++ test_patch.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.13
-KERNEL_VERSION=4.13.9
+KERNEL_VERSION=4.13.10
 
 echo "Removing old kernels..."
 rm -rf test
