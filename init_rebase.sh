dba36e07eed7597f0db7b14456c5358b8ff13b47
Matthew Ruffell <msr50@uclive.ac.nz>
2017-11-26 13:49:58 +1300

    Adding Linux 4.14.2 patches

--- init_rebase.sh
+++ init_rebase.sh
@@ -1,5 +1,5 @@
 KERNEL_MAJOR_VERSION=4.14
-KERNEL_VERSION=4.14.1
+KERNEL_VERSION=4.14.2
 
 echo "Setting up rebase directory..."
 mkdir rebase
--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.14
-KERNEL_VERSION=4.14.1
+KERNEL_VERSION=4.14.2
 
 echo "Setting up release directory..."
 mkdir release
