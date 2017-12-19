ca576196f7f81930c18724da661d83de2a26a7b7
Matthew Ruffell <msr50@uclive.ac.nz>
2017-12-20 11:51:57 +1300

    Updating to Linux 4.14.7

--- init_rebase.sh
+++ init_rebase.sh
@@ -1,5 +1,5 @@
 KERNEL_MAJOR_VERSION=4.14
-KERNEL_VERSION=4.14.6
+KERNEL_VERSION=4.14.7
 
 echo "Setting up rebase directory..."
 mkdir rebase
--- test_patch.sh
+++ test_patch.sh
@@ -1,6 +1,6 @@
 DATE=`date +%Y-%m-%d`
 KERNEL_MAJOR_VERSION=4.14
-KERNEL_VERSION=4.14.6
+KERNEL_VERSION=4.14.7
 
 echo "Removing old kernels..."
 rm -rf test
